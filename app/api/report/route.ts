25d37f474ee3a36cd655ea5f8d12be386d281eec
Jithin K Sunil <jithinksunil96@gmail.com>
2026-01-30 15:59:46 +0530

    updates

--- app/api/report/route.ts
+++ app/api/report/route.ts
@@ -32,6 +32,6 @@ export async function GET(request: NextRequest) {
       },
     });
   } catch (error) {
-    return Response.json({ message: 'Internal Server Error' }, { status: 500 });
+    return Response.json({ message: JSON.stringify(error) }, { status: 500 });
   }
 }
